e896b32ea0ecc24b2899bf37e1536adf7f951c0f
calascionec <chris.calascione@gmail.com>
2015-09-18 11:06:16 -0700

    will make it so that you can add multiple toppings at once using the arguments object

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-    $("h1").show();
-    $("button").on("click", function(){
+    $("button#start-order").on("click", function(){
+        $("button#start-order").hide();
         $(".form-area").append("<form id='select-size'>"+
                                 "<select class=''>"+
                                 "<option value='1'>" + "Small" + "</option>"+
-                                "<option value'2'>"+"Medium"+"</option>"+
+                                "<option value='2'>"+"Medium"+"</option>"+
                                 "<option value='3'>"+"Large"+"</option>"+
                                 "</select>" +"<button type='submit'>"+"Next"+"</button>"+
                                "</form>")
     });
+
+    $("form#select-size").submit(function(event){
+        event.preventDefault();
+    });
+
 });
 
-function Pizza(size, price, toppings) {
+function Pizza(size, toppings) {
     this.size = size;
-    this.price = price;
     this.toppings = toppings;
 }
 
@@ -22,6 +26,24 @@ function Topping(name, price) {
     this.price = price;
 }
 
-Pizza.prototype.addTopping = function(topping) {
-    this.toppings.push(topping);
+Pizza.prototype.addTopping = function() {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i = 0; i <args.length; i++){
+        this.toppings.push(args[i]);    
+    }
 };
+
+Pizza.prototype.getPrice = function() {
+    var price = 0;
+    if (this.size === "Small"){
+        price+= 5.99;
+    } else if (this.size === "Medium"){
+        price+= 8.99;
+    } else if (this.size === "Large"){
+        price+= 10.99;
+    }
+    for( var i = 0; i < this.toppings.length; i++) {
+        price += this.toppings[i].price;
+    }
+    return price;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -3,18 +3,33 @@ describe("Pizza", function() {
         var pepperoni = new Topping("pepperoni", .50);
         var onion = new Topping("onion", .50);
         var green_pepper = new Topping("green pepper", .50);
-        var testPizza = new Pizza("large", 10.99, [pepperoni, onion, green_pepper]);
-        expect(testPizza.size).to.equal("large");
-        expect(testPizza.price).to.equal(10.99);
+        var testPizza = new Pizza("Large", [pepperoni, onion, green_pepper]);
+        expect(testPizza.size).to.equal("Large");
         expect(testPizza.toppings).to.eql([pepperoni, onion, green_pepper]);
     });
 
     it("tests to make sure that you can add a topping to a pizza", function(){
-        var testPizza = new Pizza("large", 10.99, []);
-        var testTopping = new Topping("onion", .50);
+        var testPizza = new Pizza("Large", []);
+        var testTopping = new Topping("Onion", .50);
         testPizza.addTopping(testTopping)
         expect(testTopping).to.equal(testPizza.toppings[0]);
     });
+
+    it("test to make sure that you can add multiple toppings at once", function(){
+        var testPizza = new Pizza("Large", []);
+        var testTopping = new Topping("Onion", .50);
+        var testTopping2 = new Topping("Pepperoni", .50);
+        testPizza.addTopping(testTopping, testTopping2);
+        expect([testTopping, testTopping2]).to.eql(testPizza.toppings);
+    });
+
+    it("find cost of pizza based on toppings added", function() {
+        var testPizza = new Pizza("Large", []);
+        var testTopping = new Topping("Onion", .50);
+        var testTopping2 = new Topping("Pepperoni", .50);
+        testPizza.addTopping(testTopping, testTopping2);
+        expect(testPizza.getPrice()).to.equal(11.99);
+    })
 });
 
 describe("Topping", function() {

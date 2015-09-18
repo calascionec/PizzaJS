d33ae54eca75c4f35cd254ecc9927448f76e6eac
calascionec <chris.calascione@gmail.com>
2015-09-18 14:49:57 -0700

    update Pizza to have a quantity property

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-function Pizza(size, toppings) {
+function Pizza(size, number, toppings) {
     this.size = size;
+    this.number = number;
     this.toppings = toppings;
 }
 
@@ -18,24 +19,41 @@ Pizza.prototype.addTopping = function() {
 Pizza.prototype.getPrice = function() {
     var price = 0;
     if (this.size === "Small"){
-        price+= 5.99;
+        price = 5.99;
     } else if (this.size === "Medium"){
-        price+= 8.99;
+        price = 8.99;
     } else if (this.size === "Large"){
-        price+= 10.99;
+        price = 10.99;
     }
     for( var i = 0; i < this.toppings.length; i++) {
-        price += this.toppings[i].price;
+        price += parseFloat((this.toppings[i].price));
     }
     return price;
 }
-var sizeForm =           "<select id= 'pizza-size' >"+
+var sizeForm ="<select id= 'pizza-size' >"+
                         "<option value='Small'>" + "Small" + "</option>"+
                         "<option value='Medium'>"+"Medium"+"</option>"+
                         "<option value='Large'>"+"Large"+"</option>"+
-                        "</select>" +"<button id='pizza'>"+"Next"+"</button>";
+                        "</select>" +
+                        "<label>"+"Number Of Pizzas"+
+                        "<input type='number' id='quantity' min='1'></input>" +
+                        "<button id='pizza'>"+"Next"+"</button>";
+
+var toppingsCheckList = "<input type='checkbox' name='topping' value='.50'>" +
+                        "<label>"+"Pepperoni" +"</label>" +
+                        "<input type='checkbox' name='topping' value='.50'>" +
+                        "<label>"+"Onion" +"</label>" +
+                        "<input type='checkbox' name='topping' value='.50'>" +
+                        "<label>"+"Green Pepper" +"</label>" +
+                        "<input type='checkbox' name='topping' value='.50'>" +
+                        "<label>"+"Bacon" +"</label>" +
+                        "<button id='toppings'>"+"Next"+"</button>";
+
+
 var size;
+var number;
 var newPizza;
+var chosenToppings = [];
 $(document).ready(function() {
 
     $("button#start-order").click(function(){
@@ -44,8 +62,28 @@ $(document).ready(function() {
 
         $("button#pizza").click(function(){
             size = $("#pizza-size option:selected").text();
-            newPizza = new Pizza(size, []);
+            number = parseInt($("#quantity").val());
+            newPizza = new Pizza(size, number, []);
+            $(".form-area").empty();
+            $(".form-area").append(toppingsCheckList);
 
+            $("button#toppings").click(function(){
+                $("input[type='checkbox']:checked").each(function() {
+                    chosenToppings.push([$(this).next("label").text(), $(this).val()]);
+                    for(i = 0; i < chosenToppings.length; i++) {
+                        var topping = new Topping(chosenToppings[i][0], chosenToppings[i][1]);
+                        newPizza.addTopping(topping);
+                    }
+                });
+                $(".form-area").empty();
+                $("h1").text("Review your Order");
+                $(".form-area").append("<h2> You ordered </h2>" +
+                                        "<ul><li>" + newPizza.size + "</li>" +
+                                        "<li>" + newPizza.toppings[0].name+"</li>"+
+                                        "<li>" + newPizza.getPrice() + "</li>" +
+                                        "</ul>" +"<button id = 'start-over'>" +"Start Over"+"</button>");
+                    $("button#start-over").click(function(){location.reload();});
+            });
         });
     });
 
--- specs/specs.js
+++ specs/specs.js
@@ -3,20 +3,21 @@ describe("Pizza", function() {
         var pepperoni = new Topping("pepperoni", .50);
         var onion = new Topping("onion", .50);
         var green_pepper = new Topping("green pepper", .50);
-        var testPizza = new Pizza("Large", [pepperoni, onion, green_pepper]);
+        var testPizza = new Pizza("Large", 2, [pepperoni, onion, green_pepper]);
         expect(testPizza.size).to.equal("Large");
+        expect(testPizza.number).to.equal(2);
         expect(testPizza.toppings).to.eql([pepperoni, onion, green_pepper]);
     });
 
     it("tests to make sure that you can add a topping to a pizza", function(){
-        var testPizza = new Pizza("Large", []);
+        var testPizza = new Pizza("Large", 1, []);
         var testTopping = new Topping("Onion", .50);
         testPizza.addTopping(testTopping)
         expect(testTopping).to.equal(testPizza.toppings[0]);
     });
 
     it("test to make sure that you can add multiple toppings at once", function(){
-        var testPizza = new Pizza("Large", []);
+        var testPizza = new Pizza("Large", 1, []);
         var testTopping = new Topping("Onion", .50);
         var testTopping2 = new Topping("Pepperoni", .50);
         testPizza.addTopping(testTopping, testTopping2);
@@ -24,7 +25,7 @@ describe("Pizza", function() {
     });
 
     it("find cost of pizza based on toppings added", function() {
-        var testPizza = new Pizza("Large", []);
+        var testPizza = new Pizza("Large", 1, []);
         var testTopping = new Topping("Onion", .50);
         var testTopping2 = new Topping("Pepperoni", .50);
         testPizza.addTopping(testTopping, testTopping2);
